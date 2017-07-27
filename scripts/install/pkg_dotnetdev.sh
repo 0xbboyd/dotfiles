49ff2e9a4315bf28d7c0a9d9e3c396ab0dc82516
Brendan Boyd <firemound@gmail.com>
2017-07-26 21:35:14 -0700

    upadtes dotnet version

--- scripts/install/pkg_dotnetdev.sh
+++ scripts/install/pkg_dotnetdev.sh
@@ -3,4 +3,4 @@
 sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
 sudo apt-key adv --keyserver apt-mo.trafficmanager.net --recv-keys 417A0893
 sudo apt-get update
-sudo apt-get install dotnet-dev-1.0.0-preview2-003131
+sudo apt-get install dotnet-dev-2.0.0-preview1-005977
